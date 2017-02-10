ea75b534593ebf1afbfe6eea0efb0f08b7dae82e
Jonathan Davis <jod8bd@mail.missouri.edu>
2017-02-10 00:02:07 -0600

    Create Table
    In this file I created six fields in the table.

---
+++ CREATE TABLE/lab3.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS classes;
+
+CREATE TABLE classes
+(
+    name VARCHAR(50) NOT NULL,
+    department VARCHAR(4) NOT NULL,
+    course_id VARCHAR(6) NOT NULL,
+    start TIME,
+end TIME,
+days VARCHAR(2),
+PRIMARY KEY (course_id)
+);
